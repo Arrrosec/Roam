92e4a28defd8c7439f2ec65ad0317d49ebe7d145
Liandro Goagoseb <liandrogoagoseb@gmail.com>
2026-03-03 14:27:45 +0200

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/sections/Hero";
 import SocialProof from "./components/sections/SocialProof";
 import Features from "./components/sections/Features";
 import HowItWorks from "./components/sections/HowItWorks";
-import Destinations from "./components/sections/DEstinations";
+import Destinations from "./components/sections/Destinations";
 import Testimonials from "./components/sections/Testimonials";
 import Pricing from "./components/sections/Pricing";
 import CTA from "./components/sections/CTA";
